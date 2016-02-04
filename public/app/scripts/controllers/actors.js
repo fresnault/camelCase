e7c7a75e22cc28c0d4f3d11b90f514acaeb71171
samy35135 <samy.abdallah.fr@gmail.com>
2016-02-04 13:31:11 +0100

    Ajout relation actor movie movie actor movie actor...

--- public/app/scripts/controllers/actors.js
+++ public/app/scripts/controllers/actors.js
@@ -8,7 +8,7 @@
  * Controller of the camelCaseApp
  */
 angular.module('camelCaseApp')
-	.controller('actorsCtrl', function($http, $scope, ThingService, API_KEY, $routeParams) {
+	.controller('actorsCtrl', function($http, $scope, ThingService, API_KEY, $routeParams,$location) {
 
 
 		var idPerson = $routeParams.id;
@@ -28,7 +28,7 @@ angular.module('camelCaseApp')
 		})
 
 
-		$scope.selectedActor = function(selected) {
+		$scope.selectedMovie = function(selected) {
 			console.log(selected);
 			$location.url('vueFilm/'+selected.id);
 		}
