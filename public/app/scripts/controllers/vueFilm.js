3e43cf2203c1c674f3de85c2b9684723d5e84239
François Esnault <francois.esnault53@gmail.com>
2016-02-04 08:36:26 +0100

    no message

--- public/app/scripts/controllers/vueFilm.js
+++ public/app/scripts/controllers/vueFilm.js
@@ -16,14 +16,23 @@ angular.module('camelCaseApp')
 		var getMovie = 'http://api.themoviedb.org/3/movie/' + idMovie + '?api_key=' + API_KEY;
 		var getActors = 'http://api.themoviedb.org/3/movie/' + idMovie + '/credits?api_key=' + API_KEY;
 
+
 		$http.get(getMovie).then(function(res) {
 			$scope.movie = res.data;
 			console.log($scope.movie);
 		})
 
 		$http.get(getActors).then(function(res) {
-			$scope.movie.acteurs = res.data.cast;
-			console.log($scope.movie.acteurs);
+			$scope.movie.acteurs = res.data.cast.slice(0, 7);
+			$scope.movie.acteurs.forEach(function(key, value) {
+				console.log(key);
+
+				var getDetailedActor = 'http://api.themoviedb.org/3/person/' + key.id + '?api_key=' + API_KEY;
+				$http.get(getDetailedActor).then(function(res) {
+					key.biographie = res.data.biography;
+					console.log($scope.movie);
+				})
+			})
 		})
 
 		//$scope.thing = ThingService.recupererFilmBidon();
